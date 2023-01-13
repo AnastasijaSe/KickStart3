130a2aa116899dad94737631311e581cc38fb395
Anastasija.Semtsiv <anastasija.semtsiv@accenture.de>
2023-01-13 10:36:57 +0100

    die süßen

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Leute!");
-        System.out.println("Hello ihr süßen");
+        System.out.println("Hello ");
         System.out.println("Hello hallo");
         System.out.println("Hello ihr ");
 
